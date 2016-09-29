8f559c44bbc9a0f8e93915d94d63691a64097b86
Y-Mitsui <dyfomg@gmail.com>
2016-09-29 23:34:35 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Continuous-bandit',
     version='0.1.0',
     url='https://github.com/y-mitsui/continuous_bandit',
-    packages=find_packages(),
+    py_modules=['helpers','bayes_optim'],
     description='Bayesian optimizetion using gaussian process that optimized by cuda',
     install_requires=[
         "numpy >= 1.9.0",
